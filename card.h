f22ab8fce0ff6aa82181d2908bc27d0fe40e863f
Cynthia Espinoza-Arredondo <97571472+cespinozaa@users.noreply.github.com>
2024-09-23 21:21:08 -0400

    files

---
+++ card.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+
+class Card
+{
+public:
+	enum Suit { CLUB, DIAMOND, HEART, SPADE };
+	enum Value {
+		TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN,
+		KING, ACE
+	};
+
+public:
+	Card(Suit suit, Value value);
+	~Card();
+
+public:
+	std::string print();
+	bool sameSuit(Card compare);
+	int compareValue(Card compare);
+protected:
+	Suit suit;
+	Value value;
+};
